1c43a6f1005697de1bab9eea5db80536c11006a8
cordeirops <pedro.sbarainicordeiro@gmail.com>
2025-11-13 17:36:15 -0300

    fix - JupyterLab Dark theme hides some kale features

--- labextension/src/Theme.ts
+++ labextension/src/Theme.ts
@@ -43,78 +43,78 @@ export const theme = createTheme({
       styleOverrides: {
         root: {
           backgroundColor: 'var(--jp-layout-color1)',
-          color: 'var(--jp-ui-font-color1)',
+          color: 'var(--jp-ui-font-color1)'
         }
       }
     },
     MuiMenuItem: {
-       styleOverrides: {
-         root: {
-           color: 'var(--jp-ui-font-color1)',
-         }
-       }
+      styleOverrides: {
+        root: {
+          color: 'var(--jp-ui-font-color1)'
+        }
+      }
     },
     MuiIconButton: {
       styleOverrides: {
         root: {
-          color: 'var(--jp-ui-font-color1)',
-        },
-      },
+          color: 'var(--jp-ui-font-color1)'
+        }
+      }
     },
     MuiTypography: {
       styleOverrides: {
         root: {
-          color: 'var(--jp-ui-font-color1)',
-        },
-      },
+          color: 'var(--jp-ui-font-color1)'
+        }
+      }
     },
     MuiInputLabel: {
       styleOverrides: {
         root: {
           color: 'var(--jp-ui-font-color2)',
           '&.Mui-focused': {
-            color: '#2e82d7',
-          },
-        },
-      },
+            color: '#2e82d7'
+          }
+        }
+      }
     },
     MuiInputBase: {
       styleOverrides: {
         root: {
-          color: 'var(--jp-ui-font-color1)',
+          color: 'var(--jp-ui-font-color1)'
         }
       }
     },
     MuiOutlinedInput: {
       styleOverrides: {
         notchedOutline: {
-          borderColor: 'var(--jp-input-border-color)',
+          borderColor: 'var(--jp-input-border-color)'
         },
         root: {
           '&:hover:not(.Mui-focused) .MuiOutlinedInput-notchedOutline': {
-            borderColor: 'var(--jp-input-border-color)',
-          },
-        },
-      },
+            borderColor: 'var(--jp-input-border-color)'
+          }
+        }
+      }
     },
     MuiInput: {
       styleOverrides: {
         underline: {
           '&:before': {
-            borderBottomColor: 'var(--jp-input-border-color)',
+            borderBottomColor: 'var(--jp-input-border-color)'
           },
           '&:hover:not(.Mui-disabled):before': {
-            borderBottomColor: 'var(--jp-input-border-color)',
-          },
-        },
-      },
+            borderBottomColor: 'var(--jp-input-border-color)'
+          }
+        }
+      }
     },
     MuiSelect: {
       styleOverrides: {
         icon: {
-          color: 'var(--jp-ui-font-color1)',
-        },
-      },
-    },
-  },
+          color: 'var(--jp-ui-font-color1)'
+        }
+      }
+    }
+  }
 });
